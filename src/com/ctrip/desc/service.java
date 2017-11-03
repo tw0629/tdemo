1b2ff40fd53466703ce5fb0701b2ab8245f39e65
tian <tianwei0629@126.com>
2017-11-03 14:02:44 +0800

    2

--- src/com/ctrip/desc/service.java
+++ src/com/ctrip/desc/service.java
@@ -5,6 +5,8 @@ public class service {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("hello everybody");
+          System.out.println("2:UIOP");
+          
 	}
 
 }
